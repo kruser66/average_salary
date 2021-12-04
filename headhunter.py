6aaf03b0252d9790987d5f3753fc7a2ea5da80ec
kruser <kruser@yandex.ru>
2021-12-04 14:02:14 +0500

    Update headhunter.py
    Solved item 4 of the task

--- headhunter.py
+++ headhunter.py
@@ -18,6 +18,24 @@ def search_vacancies(search_text=''):
     return data['found']
 
 
+def fetch_salary(search_text='Разработчик Pyhon'):
+    api_url = 'https://api.hh.ru/vacancies'
+
+    params = {
+        'area': 1,
+        'text': search_text,
+        'search_field': 'name',
+        # 'per_page': 0,
+        # 'clusters': True,
+    }
+    response = requests.get(api_url, params=params)
+    data = response.json()['items']
+
+    list_salary = [elem['salary'] for elem in data]
+
+    return list_salary
+
+
 if __name__ == '__main__':
     code_lauguages = [
         'Java',
@@ -35,7 +53,9 @@ if __name__ == '__main__':
 
     search_result = {}
 
-    for code_lauguage in code_lauguages:
-        search_result[code_lauguage] = search_vacancies(code_lauguage)
+    # # for code_lauguage in code_lauguages:
+    # #     search_result[code_lauguage] = search_vacancies(code_lauguage)
 
-    pprint(search_result)
+    # pprint(search_result)
+
+    pprint(fetch_salary())
