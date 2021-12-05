1bb71909f02b7260387558cf970efd3d854e5411
kruser <kruser@yandex.ru>
2021-12-05 14:49:30 +0500

    Resrtuctured modules
    add search by SuperJob
    Item 11 of the task

---
+++ launguage_salary.py
@@ -0,0 +1,142 @@
+import os
+import requests
+from pprint import pprint
+from dotenv import load_dotenv
+
+
+def search_vacancies_hh(search_text, area, page=0, clusters=False, search_field=''):
+    api_url = 'https://api.hh.ru/vacancies'
+
+    params = {
+        'area': area,
+        'text': search_text,
+        'search_field': search_field,
+        'page': page,
+        'clusters': clusters,
+    }
+
+    response = requests.get(api_url, params=params)
+    if response.ok:
+        return response.json()
+    else:
+        response.raise_for_status()
+        return {'items':[]}
+
+
+def search_vacancies_sj(secret_key, search_text, town, page=0):
+    url = 'https://api.superjob.ru/2.0/vacancies/'
+    
+    headers = {
+        'X-Api-App-Id': secret_key,
+    }
+    params = {
+        'town': town,
+        'page': page,
+        'keywords': [[1, 'and', search_text]],
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    if response.ok:
+        return response.json()
+    else:
+        response.raise_for_request()
+        return {'objects': []}
+
+
+def predict_salary(payment_from, payment_to, currency):
+    if currency == 'rub' or currency == 'RUR':
+        if payment_from and payment_to:
+            return int((payment_from + payment_to) / 2)
+        elif payment_from:
+            return int(payment_from * 1.2)
+        elif payment_to:
+            return int(payment_to * 0.8)
+
+
+def predict_rub_salary_hh(vacancy):
+    salary = vacancy['salary']
+    payment_from = salary['from']
+    payment_to = salary['to']
+    currency = salary['currency']
+
+    if salary:
+        return predict_salary(payment_from, payment_to, currency)
+
+
+def predict_rub_salary_sj(vacancy):
+    payment_from = vacancy['payment_from']
+    payment_to = vacancy['payment_to']
+    currency = vacancy['currency']
+    
+    return predict_salary(payment_from, payment_to, currency)
+
+
+def collect_average_salary_hh(code_lauguages, town):
+    search_result = {}
+
+    for code in code_lauguages:
+        search_result[code] = {}
+
+        vacancies = search_vacancies_hh(search_text=f'Разработчик {code}', area=town, search_field='name')
+        salaries = [predict_rub_salary_hh(vacancy) for vacancy in vacancies['items']]
+
+        for page in range(1,vacancies['pages'] + 1):
+            vacancies = search_vacancies_hh(search_text=f'Разработчик {code}', area=town, page=page, search_field='name')
+            salaries.extend([predict_rub_salary_hh(vacancy) for vacancy in vacancies['items']])
+        
+        search_result[code]['vacancies_found'] = len(salaries)
+        salaries = [elem for elem in salaries if elem and elem > 20000]
+
+        search_result[code]['vacancies_processed'] = len(salaries)
+        search_result[code]['average_salary'] = int(sum(salaries) / len(salaries))
+        
+    return search_result
+
+
+def collect_average_salary_sj(secret_key, code_lauguages, town):
+    search_result = {}
+
+    for code in code_lauguages:
+        search_result[code] = {}
+        vacancies = search_vacancies_sj(secret_key, search_text=f'Разработчик {code}', town=town)
+        salaries = [predict_rub_salary_sj(vacancy) for vacancy in vacancies['objects']]
+
+        page = 1
+        while vacancies['more']:
+            vacancies = search_vacancies_sj(secret_key, search_text=f'Разработчик {code}', town=town, page=page)
+            salaries.extend([predict_rub_salary_sj(vacancy) for vacancy in vacancies['objects']])
+            page += 1
+        
+        search_result[code]['vacancies_found'] = len(salaries)
+        salaries = [elem for elem in salaries if elem and elem > 20000]
+
+        search_result[code]['vacancies_processed'] = len(salaries)
+        search_result[code]['average_salary'] = int(sum(salaries) / len(salaries))
+        
+    return search_result
+
+
+if __name__ == '__main__':
+
+    load_dotenv()
+    secret_key = os.getenv('SECRET_KEY')
+
+    area = 1 # id региона или населенного пункта https://api.hh.ru/areas для поиска по HeadHunter 1- Москва
+    town = 'Москва' # Имя города или id для поиска по SuperJob - id смотреть тут: https://api.superjob.ru/2.0/regions/combined/
+
+    code_lauguages = [
+        'Java',
+        'JavaScript',
+        'Python',
+        'Ruby',
+        'PHP',
+        'C++',
+        'C#',
+        'Swift',
+        'TypeScript',
+        'Go',
+        'Scala',
+    ]
+
+    pprint(collect_average_salary_hh(code_lauguages, area))
+    pprint(collect_average_salary_sj(secret_key, code_lauguages, town))
